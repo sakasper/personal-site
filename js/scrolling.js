522e396b5994905d470f357a660706d034b038c7
sakasper <scott@sakasper.com>
2018-01-09 10:44:24 -0500

    added scrolling

---
+++ js/scrolling.js
@@ -0,0 +1,43 @@
+var controller = new ScrollMagic.Controller();
+var scrollDuration = 750;
+
+function addSmoothScrolling () {
+	controller.scrollTo(function (target, params) {
+		var duration = params.duration;
+		var animateSkillSection = params.animateSkills;
+		$('html,body').animate({scrollTop : target}, duration);
+		if(animateSkillSection){
+			animateSkills();
+		}
+	});
+
+	$('a').each(function (i, elem) {
+		var target = elem.hash;
+		elem.onclick = function (event) {
+			if(target !== "") {
+				event.preventDefault();
+				var animateSkills = (target == "#skills");
+				var params = {
+					duration: scrollDuration,
+					animateSkills: animateSkills
+				};
+				controller.scrollTo(target, params);
+			}
+		}
+	});
+}
+
+function addNavFade () {
+	var navbarScrollFade = new ScrollMagic.Scene({
+		triggerElement: "#skills",
+		triggerHook: "onEnter",
+		offset: 20,
+	})
+	.setClassToggle("#nav", "scrolled")
+	.addTo(controller);
+}
+
+function initializeScrolling () {
+	addNavFade();
+	addSmoothScrolling();
+}

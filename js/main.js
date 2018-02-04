ea8bbd81b85dbd90c56c7220276b294dad69b5e1
mjkasper <mkasper@andrew.cmu.edu>
2018-02-04 03:13:13 -0500

    made some changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+function smallScreenRedirectIfNeeded() {
+	if(($(window).width() < 1300)) {
+		window.location="small-screen.html";
+	}
+}
+
 $(document).ready(function () {
 	initializeScrolling();
+	smallScreenRedirectIfNeeded();
 });
